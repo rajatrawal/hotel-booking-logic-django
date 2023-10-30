95b7cee84b5faae4f04b71ad67547f3633e466a9
Rajat <rwlrajat@gmail.com>
2023-10-30 08:09:57 +0530

    initial commit

--- home/urls.py
+++ home/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('hotel/<uid>',get_hotel,name='get_hotel'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     
 urlpatterns += staticfiles_urlpatterns()
